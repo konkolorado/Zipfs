dff35d7f7cf0fa83e2fe6d2549ea6b7d19914bdd
urielmandujano <uriel.mandujano14@gmail.com>
2016-10-30 12:55:59 -0500

    Added randomlang graphs

--- zipfs.py
+++ zipfs.py
@@ -15,10 +15,9 @@ def main():
     es = wikipedia_language(84, "https://es.wikipedia.org/wiki/Bogosort", False)
     fr = wikipedia_language(497, "https://fr.wikipedia.org/wiki/Tri_stupide", False)
     de = wikipedia_language(490, "https://de.wikipedia.org/wiki/Bogosort", False)
+    rand = random_language(1000000)
 
-    loglangs([en, es, fr, de])
-    # TODO
-    # figure out if randomlang follows Zipfian Distribution
+    loglangs([en, es, fr, de, rand])
 
 def wikipedia_language(num_pages, start_url, wrangle, graph=False):
     wrangler = WordWrangler(num_pages, start_url)
@@ -31,21 +30,23 @@ def wikipedia_language(num_pages, start_url, wrangle, graph=False):
         graph_it(10, words, frequencies)
     return words, frequencies
 
-def random_language():
-    random_words = make_random(500000, "abcdefghijklmnopqrstuvwxyz")
+def random_language(num_words, graph=False):
+    random_words = make_random(num_words, "abcdefghijklmnopqrstuvwxyz")
     rand_sorted_words = sort_by_frequency(random_words)
     rand_w, rand_f = split_sorted(rand_sorted_words)
-    graph_it(10, rand_w, rand_f)
+    if graph:
+        graph_it(10, rand_w, rand_f)
+    return rand_w, rand_f
 
 def sort_by_frequency(d):
     """
-    Given a dictionary containing word:freq info, sort on freq value
+    given a dictionary containing word:freq info, sort on freq value
     """
     return sorted(d.items(), key=operator.itemgetter(1), reverse=True)
 
 def split_sorted(ls):
     """
-    Given a list of tuples, returns 2 lists containing the tuple
+    given a list of tuples, returns 2 lists containing the tuple
     info in a 1-to-1 correspondence
     """
     return zip(*ls)
@@ -57,11 +58,11 @@ def graph_it(num_items, x_items, y_items):
     opacity = 0.4
 
     rects1 = plt.bar(index + bar_width / 2, y_items[:num_items], bar_width,
-                     alpha=opacity, color='b', label='Words')
+                     alpha=opacity, color='b', label='words')
 
-    plt.xlabel('Words')
-    plt.ylabel('Frequencies')
-    plt.title('Distribution of words on wikipedia')
+    plt.xlabel('rank')
+    plt.ylabel('frequency')
+    plt.title('distribution of words on wikipedia')
     plt.xticks(index + bar_width, x_items[:num_items])
     plt.show()
 
@@ -70,11 +71,11 @@ def loglog(num_items, x_items, y_items):
     bar_width = 0.5
     opacity = 0.4
 
-    plt.xlabel('Word rank (log)')
-    plt.ylabel('Frequency (log)')
-    plt.title('Distribution of words on wikipedia')
+    plt.xlabel('log(rank)')
+    plt.ylabel('log(frequency)')
+    plt.title('distribution of words on wikipedia')
     xs = range(1, len(x_items)+1)
-    plt.scatter(np.log10(xs), np.log10(y_items), s=100, c='b', alpha=0.5)
+    plt.scatter(np.log2(xs), np.log2(y_items), s=100, c='b', alpha=0.5)
     plt.show()
 
 def plotlogs(x_items, y_items, color):
@@ -83,17 +84,17 @@ def plotlogs(x_items, y_items, color):
 
 def loglangs(langs_list):
     """
-    Parameters: A list of language tuples: [ (words, frequencies), ...]
-    Plots language statistics
+    parameters: a list of language tuples: [ (words, frequencies), ...]
+    plots language statistics
     """
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
     if len(colors) < len(langs_list):
-        print "Not enough colors for each lang, reduce langs"
+        print "not enough colors for each lang, reduce langs"
         return
 
-    plt.xlabel("Word rank(log)")
-    plt.ylabel("Frequency (log)")
-    plt.title("Distribution of words in various languages on Wikipedia")
+    plt.xlabel("log(rank)")
+    plt.ylabel("log(frequency)")
+    plt.title("distribution of words in various languages on wikipedia")
 
     for lang in langs_list:
         words, freqs = lang
@@ -102,8 +103,8 @@ def loglangs(langs_list):
 
 def make_random(v_size, alphabet):
     """
-    Given a string of alphabet characters, produce a random vocabulary
-    of words of size v_size. A white space char indicates the end of
+    given a string of alphabet characters, produce a random vocabulary
+    of words of size v_size. a white space char indicates the end of
     a word
     """
     random_words = {}
@@ -117,7 +118,7 @@ def make_word(alphabet):
     word = ""
     while True:
         let = random.choice(alphabet)
-        if let == " " and word != "":
+        if let == " " and len(word) > 0:
             return word
         if let == " ":
             continue
